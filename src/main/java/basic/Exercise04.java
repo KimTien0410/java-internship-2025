45e39541f820e0247be35db25791ccc899051bb4
tientran <kimtientran0410@gmail.com>
2025-08-26 11:56:04 +0700

    edit class Exercise04 with space and edit code

--- src/main/java/basic/Exercise04.java
+++ src/main/java/basic/Exercise04.java
@@ -1,25 +1,27 @@
 package basic;
 
 public class Exercise04 {
-    public int fibonacci(int n) {
+
+    public int fibonacci(final int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Input cannot be negative");
         }
-        if (n == 0) {
-            return 0;
+        if (n <= 1) {
+            return n;
         }
-        if (n == 1) {
-            return 1;
-        }
+
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
+
     public String fibonacciArray(int n) {
+
         if (n < 0) {
             throw new IllegalArgumentException("Input cannot be negative");
         }
-        StringBuffer result = new StringBuffer();
-       int i=1;
-        while(fibonacci(i)<n){
+        final StringBuffer result = new StringBuffer();
+       int i = 1;
+
+        while( fibonacci(i) < n ){
             result.append(fibonacci(i)).append(" ");
             i++;
         }
