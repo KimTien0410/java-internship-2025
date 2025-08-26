294d6ab60e312571afc18bab3547024d995da0e9
tientran <kimtientran0410@gmail.com>
2025-08-26 15:56:52 +0700

    Feature string exercise02

---
+++ src/main/java/string/Exercise02.java
@@ -0,0 +1,22 @@
+package string;
+
+public class Exercise02 {
+    public int sumNumbers(String str) {
+        int sum = 0;
+        final StringBuilder builder = new StringBuilder();
+        for(char c : str.toCharArray()) {
+            if(Character.isDigit(c)) {
+                builder.append(c);
+            } else {
+                if(!builder.isEmpty()) {
+                    sum += Integer.parseInt(builder.toString());
+                    builder.setLength(0);
+                }
+            }
+        }
+        if(!builder.isEmpty()) {
+            sum += Integer.parseInt(builder.toString());
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/string/Exercise02Test.java
@@ -0,0 +1,20 @@
+package string;
+
+import org.junit.jupiter.api.Test;
+
+public class Exercise02Test {
+    @Test
+    public void sumNumberTest_OK(){
+        final Exercise02 exercise02 = new Exercise02();
+        assert(exercise02.sumNumbers("abc 123 xyz") == 123);
+        assert(exercise02.sumNumbers("aa 11 b 33") == 44);
+        assert(exercise02.sumNumbers("7 11") == 18);
+        assert(exercise02.sumNumbers("Chocolate") == 0);
+        assert(exercise02.sumNumbers("5hoco1a1e") == 7);
+        assert(exercise02.sumNumbers("") == 0);
+        assert(exercise02.sumNumbers("5$$1;;1!!") == 7);
+        assert(exercise02.sumNumbers("a1234bb11") == 1245);
+        assert(exercise02.sumNumbers("12ab30bc7") == 49);
+        assert(exercise02.sumNumbers("12.4ab3.0bc.7") == 26);
+    }
+}
