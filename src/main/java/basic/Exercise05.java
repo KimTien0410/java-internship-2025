27ae683a927520f22ffc6fa6f9271630e5b9f22d
tientran <kimtientran0410@gmail.com>
2025-08-26 11:59:04 +0700

    edit file Exercise05 with line space and blank space

--- src/main/java/basic/Exercise05.java
+++ src/main/java/basic/Exercise05.java
@@ -1,11 +1,13 @@
 package basic;
 
 public class Exercise05 {
-    public int factorial(int number) {
-        if (number < 0) {
+
+    public int factorial(final int number) {
+
+        if ( number < 0 ) {
             throw new IllegalArgumentException("Number must be non-negative");
         }
+
         return number == 0 ? 1 : number * factorial(number - 1);
     }
-
 }
