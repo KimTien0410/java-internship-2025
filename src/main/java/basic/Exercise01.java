9af9f681aa0e8b4af5906cc93810e02445b962f5
tientran <kimtientran0410@gmail.com>
2025-08-26 09:25:03 +0700

    fix basic exercise01 format code with convention code

--- src/main/java/basic/Exercise01.java
+++ src/main/java/basic/Exercise01.java
@@ -1,11 +1,13 @@
 package basic;
 
 public class Exercise01 {
-    public int GCD(int a,int b){
-        if(b==0) return a;
-        return GCD(b,a%b);
+    public int getGCD(int a, int b) {
+        if (b == 0) return a;
+
+        return getGCD(b, a % b);
     }
-    public int LCM(int a,int b){
-        return a*b/GCD(a,b);
+
+    public int getLCM(int a, int b) {
+        return a * b / getGCD(a, b);
     }
 }
