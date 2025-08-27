6e4c1a6a3cde431815334ef9e64ac28f572f273f
tientran <kimtientran0410@gmail.com>
2025-08-27 11:44:36 +0700

    Feature string exercise04

---
+++ src/main/java/string/Exercise04.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Exercise04 {
+    public String cutString(final String str){
+        HashMap<String,Integer> map=new HashMap<>();
+        StringBuilder result=new StringBuilder();
+        for(Character c:str.toCharArray()){
+            map.put(c.toString(),map.getOrDefault(c.toString(),0)+1);
+        }
+        for(Map.Entry<String,Integer> entry:map.entrySet()){
+            if(entry.getValue()>1)
+                result.append(entry.getKey()).append(entry.getValue());
+            else{
+                result.append(entry.getKey());
+                continue;
+            }
+        }
+        return result.toString();
+    }
+}
