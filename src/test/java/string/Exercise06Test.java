8c7aae207c2ee93db09e8692d5506651a5065d60
tientran <kimtientran0410@gmail.com>
2025-08-29 17:09:53 +0700

    Add white line and use assertTrue and assertFalse instead of assert

--- src/test/java/string/Exercise06Test.java
+++ src/test/java/string/Exercise06Test.java
@@ -2,19 +2,22 @@ package string;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Exercise06Test {
     @Test
     void shouldCheckBarcode_OK() {
         final Exercise06 exercise06 = new Exercise06();
-        assert exercise06.checkBarcode("4006381333931");
-        assert exercise06.checkBarcode("9780306406157");
-        assert exercise06.checkBarcode("9781861972712");
-        assert !exercise06.checkBarcode("1234567890129");
-        assert exercise06.checkBarcode("5901234123457");
-        assert !exercise06.checkBarcode("4006381333932");
-        assert !exercise06.checkBarcode("400638133393");
-        assert !exercise06.checkBarcode("40063813339312");
-        assert !exercise06.checkBarcode("40063813339A1");
-        assert !exercise06.checkBarcode(null);
+        assertTrue(exercise06.checkBarcode("4006381333931"));
+        assertTrue(exercise06.checkBarcode("9780306406157"));
+        assertTrue(exercise06.checkBarcode("9781861972712"));
+        assertFalse(exercise06.checkBarcode("1234567890129"));
+        assertTrue(exercise06.checkBarcode("5901234123457"));
+        assertFalse(exercise06.checkBarcode("4006381333932"));
+        assertFalse(exercise06.checkBarcode("400638133393"));
+        assertFalse(exercise06.checkBarcode("40063813339312"));
+        assertFalse(exercise06.checkBarcode("40063813339A1"));
+        assertFalse(exercise06.checkBarcode(null));
     }
 }
