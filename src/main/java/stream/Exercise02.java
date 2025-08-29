8c5abc07d1dcc617a2183383fab72a46c5f0138b
tientran <kimtientran0410@gmail.com>
2025-08-29 17:46:01 +0700

    Modify code line 7, 8

--- src/main/java/stream/Exercise02.java
+++ src/main/java/stream/Exercise02.java
@@ -4,6 +4,7 @@ import static java.util.Arrays.stream;
 
 public class Exercise02 {
     public int findNumberNotInArray(int[] array){
-        return ((1 + array.length + 1) * (array.length + 1) / 2) -  stream(array).sum();
+        int length = array.length;
+        return ((length + 1) * (length + 2) / 2) -  stream(array).sum();
     }
 }
