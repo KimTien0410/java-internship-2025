403f3aa9746802b91c774ec4c7144d3e53b750ff
tientran <kimtientran0410@gmail.com>
2025-08-29 15:38:09 +0700

    Use assertTrue and assertFalse change assert

--- src/test/java/string/Exercise01Test.java
+++ src/test/java/string/Exercise01Test.java
@@ -2,19 +2,22 @@ package string;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Exercise01Test {
 
     @Test
     void shouldCheckSymmetric_OK() {
         final Excersice01 excersice = new Excersice01();
-        assert excersice.isSymmetric("madam");
-        assert excersice.isSymmetric("racecar");
-        assert excersice.isSymmetric("a");
-        assert excersice.isSymmetric("");
-        assert excersice.isSymmetric(null);
-        assert !excersice.isSymmetric("hello");
-        assert !excersice.isSymmetric("abc");
-        assert !excersice.isSymmetric("ab");
+        assertTrue(excersice.isSymmetric("madam"));
+        assertTrue(excersice.isSymmetric("racecar"));
+        assertTrue(excersice.isSymmetric("a"));
+        assertTrue(excersice.isSymmetric(""));
+        assertTrue(excersice.isSymmetric(null));
+        assertFalse(excersice.isSymmetric("hello"));
+        assertFalse(excersice.isSymmetric("abc"));
+        assertFalse(excersice.isSymmetric("ab"));
 
     }
 }
