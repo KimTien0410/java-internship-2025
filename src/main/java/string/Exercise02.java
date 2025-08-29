0e6337b244727258c6241a21e2f9e6e8ae240b37
tientran <kimtientran0410@gmail.com>
2025-08-29 16:11:32 +0700

    Use variable current change StringBuilder

--- src/main/java/string/Exercise02.java
+++ src/main/java/string/Exercise02.java
@@ -4,20 +4,16 @@ public class Exercise02 {
 
     public int sumNumbers(String str) {
         int sum = 0;
-        final StringBuilder builder = new StringBuilder();
-        for(char c : str.toCharArray()) {
-            if(Character.isDigit(c)) {
-                builder.append(c);
+        int current = 0;
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                current = current * 10 + (c - '0');
             } else {
-                if(!builder.isEmpty()) {
-                    sum += Integer.parseInt(builder.toString());
-                    builder.setLength(0);
-                }
+                sum += current;
+                current = 0;
             }
         }
-        if(!builder.isEmpty()) {
-            sum += Integer.parseInt(builder.toString());
-        }
+        sum += current;
         return sum;
     }
 }
--- src/test/java/string/Exercise02Test.java
+++ src/test/java/string/Exercise02Test.java
@@ -2,19 +2,21 @@ package string;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Exercise02Test {
     @Test
-    public void sumNumberTest_OK(){
+    public void shouldSumNumber_OK(){
         final Exercise02 exercise02 = new Exercise02();
-        assert(exercise02.sumNumbers("abc123xyz") == 123);
-        assert(exercise02.sumNumbers("aa11b33") == 44);
-        assert(exercise02.sumNumbers("7 11") == 18);
-        assert(exercise02.sumNumbers("Chocolate") == 0);
-        assert(exercise02.sumNumbers("5hoco1a1e") == 7);
-        assert(exercise02.sumNumbers("") == 0);
-        assert(exercise02.sumNumbers("5$$1;;1!!") == 7);
-        assert(exercise02.sumNumbers("a1234bb11") == 1245);
-        assert(exercise02.sumNumbers("12ab30bc7") == 49);
-        assert(exercise02.sumNumbers("12.4ab3.0bc.7") == 26);
+        assertEquals(123, exercise02.sumNumbers("abc123xyz"));
+        assertEquals(44, exercise02.sumNumbers("aa11b33"));
+        assertEquals(18, exercise02.sumNumbers("7 11"));
+        assertEquals(0, exercise02.sumNumbers("Chocolate"));
+        assertEquals(7, exercise02.sumNumbers("5hoco1a1e"));
+        assertEquals(0, exercise02.sumNumbers(""));
+        assertEquals(7, exercise02.sumNumbers("5$$1;;1!!"));
+        assertEquals(1245, exercise02.sumNumbers("a1234bb11"));
+        assertEquals(49, exercise02.sumNumbers("12ab30bc7"));
+        assertEquals(26, exercise02.sumNumbers("12.4ab3.0bc.7"));
     }
 }
