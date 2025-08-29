082dc71165f6f4e0ad0d41f0ce9a511ab396651e
tientran <kimtientran0410@gmail.com>
2025-08-29 16:44:06 +0700

    Fix space and remove continue

--- src/main/java/string/Exercise04.java
+++ src/main/java/string/Exercise04.java
@@ -4,18 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Exercise04 {
-    public String cutString(final String str){
-        HashMap<String,Integer> map=new HashMap<>();
-        StringBuilder result=new StringBuilder();
-        for(Character c:str.toCharArray()){
-            map.put(c.toString(),map.getOrDefault(c.toString(),0)+1);
+    public String cutString(final String str) {
+        HashMap<String, Integer> map = new HashMap<>();
+        StringBuilder result = new StringBuilder();
+        for (Character c : str.toCharArray()) {
+            map.put(c.toString(), map.getOrDefault(c.toString(), 0) + 1);
         }
-        for(Map.Entry<String,Integer> entry:map.entrySet()){
-            if(entry.getValue()>1)
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1)
                 result.append(entry.getKey()).append(entry.getValue());
-            else{
+            else {
                 result.append(entry.getKey());
-                continue;
             }
         }
         return result.toString();
