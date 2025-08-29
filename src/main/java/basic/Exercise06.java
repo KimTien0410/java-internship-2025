c0c53353d76b558d40f109983ae502edba4fd975
tientran <kimtientran0410@gmail.com>
2025-08-29 15:02:49 +0700

    Use Array change switch case and use assertEquals change assert

--- src/main/java/basic/Exercise06.java
+++ src/main/java/basic/Exercise06.java
@@ -3,20 +3,10 @@ package basic;
 public class Exercise06 {
 
     public String readMonth(final int month) {
-        return switch (month) {
-            case 1 -> "January";
-            case 2 -> "February";
-            case 3 -> "March";
-            case 4 -> "April";
-            case 5 -> "May";
-            case 6 -> "June";
-            case 7 -> "July";
-            case 8 -> "August";
-            case 9 -> "September";
-            case 10 -> "October";
-            case 11 -> "November";
-            case 12 -> "December";
-            default -> "Invalid month";
+        final String[] months = {
+                "Invalid month", "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
         };
+        return months[month];
     }
 }
--- src/test/java/basic/Exercise06Test.java
+++ src/test/java/basic/Exercise06Test.java
@@ -2,23 +2,24 @@ package basic;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Exercise06Test {
     @Test
     void shouldReadMonth_OK() {
         final Exercise06 exercise06 = new Exercise06();
-        assert "January".equals(exercise06.readMonth(1));
-        assert "February".equals(exercise06.readMonth(2));
-        assert "March".equals(exercise06.readMonth(3));
-        assert "April".equals(exercise06.readMonth(4));
-        assert "May".equals(exercise06.readMonth(5));
-        assert "June".equals(exercise06.readMonth(6));
-        assert "July".equals(exercise06.readMonth(7));
-        assert "August".equals(exercise06.readMonth(8));
-        assert "September".equals(exercise06.readMonth(9));
-        assert "October".equals(exercise06.readMonth(10));
-        assert "November".equals(exercise06.readMonth(11));
-        assert "December".equals(exercise06.readMonth(12));
-        assert "Invalid month".equals(exercise06.readMonth(0));
-        assert "Invalid month".equals(exercise06.readMonth(13));
+        assertEquals("January", exercise06.readMonth(1));
+        assertEquals("February", exercise06.readMonth(2));
+        assertEquals("March", exercise06.readMonth(3));
+        assertEquals("April", exercise06.readMonth(4));
+        assertEquals("May", exercise06.readMonth(5));
+        assertEquals("June", exercise06.readMonth(6));
+        assertEquals("July", exercise06.readMonth(7));
+        assertEquals("August", exercise06.readMonth(8));
+        assertEquals("September", exercise06.readMonth(9));
+        assertEquals("October", exercise06.readMonth(10));
+        assertEquals("November", exercise06.readMonth(11));
+        assertEquals("December", exercise06.readMonth(12));
+        assertEquals("Invalid month", exercise06.readMonth(0));
     }
 }
