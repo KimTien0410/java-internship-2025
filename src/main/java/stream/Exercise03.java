45a8880fe78dc356d6492858e0328e91c8429d32
tientran <kimtientran0410@gmail.com>
2025-08-29 17:49:13 +0700

    Add space and move names array to class

--- src/main/java/stream/Exercise03.java
+++ src/main/java/stream/Exercise03.java
@@ -3,11 +3,12 @@ package stream;
 import java.util.Arrays;
 
 public class Exercise03 {
-    public String readNumber(final int number){
-        String[] names={"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+    String[] names = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+
+    public String readNumber(final int number) {
         return Arrays.stream(String.valueOf(number).split(""))
                 .map(digit -> names[Integer.parseInt(digit)])
-                .reduce((a,b)->a+", "+b)
+                .reduce((a, b) -> a + ", " + b)
                 .orElse("");
     }
 }
