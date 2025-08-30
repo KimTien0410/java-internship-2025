3c2c885434069e32b95c2d8a2cf64ff574f0dc52
tientran <kimtientran0410@gmail.com>
2025-08-30 16:18:03 +0700

    Declare a variable on a line

--- src/main/java/basic/Exercise04.java
+++ src/main/java/basic/Exercise04.java
@@ -3,7 +3,8 @@ package basic;
 public class Exercise04 {
     public String fibonacciArray(int max) {
         StringBuilder result = new StringBuilder();
-        int a = 1, b = 1;
+        int a = 1;
+        int b = 1;
         result.append(a);
         while (b < max) {
             result.append(" ").append(b);
