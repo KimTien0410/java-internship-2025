f2e04f1b817178dcfe40a52c1ab55bcbeffd9e86
tientran <kimtientran0410@gmail.com>
2025-08-30 16:36:35 +0700

    move months variable to class

--- src/main/java/basic/Exercise06.java
+++ src/main/java/basic/Exercise06.java
@@ -1,12 +1,24 @@
 package basic;
 
 public class Exercise06 {
+    final String[] months = {
+            "Invalid month",
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+    };
 
     public String readMonth(final int month) {
-        final String[] months = {
-                "Invalid month", "January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December"
-        };
+
         return months[month];
     }
 }
