3afa21ca1fcce4aa49772c017c7a6518294f8c5d
tientran <kimtientran0410@gmail.com>
2025-08-30 16:31:33 +0700

    move romanNumerals and values to class

--- src/main/java/basic/Exercise08.java
+++ src/main/java/basic/Exercise08.java
@@ -1,20 +1,21 @@
 package basic;
 
 public class Exercise08 {
+    String[] romanNumerals = {
+            "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+    };
+
+    int[] values = {
+            1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+    };
+
     public String toRomanNumerals(int number) {
         if (number <= 0 || number > 3999) {
             throw new IllegalArgumentException("Number out of range (1-3999)");
         }
 
-        String[] romanNumerals = {
-            "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
-        };
-
-        int[] values = {
-            1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
-        };
-
         StringBuilder roman = new StringBuilder();
+
         for (int i = 0; i < values.length; i++) {
             while (number >= values[i]) {
                 roman.append(romanNumerals[i]);
