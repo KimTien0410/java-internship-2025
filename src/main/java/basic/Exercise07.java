397fa9c2ee9348d989bffb05a6e08ed3ce5e9b74
tientran <kimtientran0410@gmail.com>
2025-09-02 19:41:39 +0700

    make simple toBinary method

--- src/main/java/basic/Exercise07.java
+++ src/main/java/basic/Exercise07.java
@@ -2,17 +2,7 @@ package basic;
 
 public class Exercise07 {
     public String toBinary(int n) {
-        final StringBuilder binary = new StringBuilder();
-
-        if (n == 0) {
-            return "0";
-        }
-
-        while (n > 0) {
-            binary.insert(0, n % 2);
-            n /= 2;
-        }
-        return binary.toString();
+        return Integer.toBinaryString(n);
     }
 
     public int toDecimalReverse(int n) {
