8503662ef4dbfbae2d91bd12519e9c125647a12d
tientran <kimtientran0410@gmail.com>
2025-09-03 10:13:00 +0700

    Optimize code basic exercise03

--- src/main/java/basic/Exercise03.java
+++ src/main/java/basic/Exercise03.java
@@ -2,29 +2,19 @@ package basic;
 
 public class Exercise03 {
 
-    public boolean isPrime(int number) {
-        if(number <= 1){
-            return false;
-        }
-        for(int i = 2; i * i <= number; i++){
-            if(number % i == 0){
-                return false;
-            }
-        }
-        return true;
-    }
-
     public String analyzeNumber(int number) {
         StringBuffer sb = new StringBuffer();
-        for(int i = 2; i <= number; i++){
-            if(number % i == 0 && isPrime(i)) {
-                while(number % i == 0){
-                    number /= i;
-                    sb.append(i).append("*");
-                }
+        for(int i = 2; i * i <= number; i++){
+            while(number % i == 0){
+                number /= i;
+                sb.append(i).append("*");
             }
         }
+        if(number > 1) {
+            sb.append(number).append("*");
+        }
         sb.deleteCharAt(sb.length()-1);
+
         return sb.toString();
     }
 }
--- src/test/java/basic/Exercise03Test.java
+++ src/test/java/basic/Exercise03Test.java
@@ -8,6 +8,11 @@ public class Exercise03Test {
     @Test
     void testAnalyzeNumber() {
         final Exercise03 exercise03 = new Exercise03();
+        assertEquals("2*2*3*5", exercise03.analyzeNumber(60));
+        assertEquals("3*7", exercise03.analyzeNumber(21));
+        assertEquals("2*2*2*3*5*5", exercise03.analyzeNumber(600));
+        assertEquals("7", exercise03.analyzeNumber(7));
+        assertEquals("13", exercise03.analyzeNumber(13));
         assertEquals("2*11", exercise03.analyzeNumber(22));
         assertEquals("2*2*2*3", exercise03.analyzeNumber(24));
     }
