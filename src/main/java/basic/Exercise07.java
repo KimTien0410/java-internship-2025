72d45a078d5e5fd2b1d78217a480fe8b0ee57ff8
tientran <kimtientran0410@gmail.com>
2025-09-03 09:37:10 +0700

    Use for loop change Interger.toBinary()

--- src/main/java/basic/Exercise07.java
+++ src/main/java/basic/Exercise07.java
@@ -2,7 +2,12 @@ package basic;
 
 public class Exercise07 {
     public String toBinary(int n) {
-        return Integer.toBinaryString(n);
+        if (n == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        for(int i = n; i > 0; i /= 2) {
+            sb.append(i % 2);
+        }
+        return sb.reverse().toString();
     }
 
     public int toDecimalReverse(int n) {
