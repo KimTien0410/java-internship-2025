d0cdf42b09c9b6b1c96a512477dd2c0abcd50ff8
tientran <kimtientran0410@gmail.com>
2025-09-03 14:52:46 +0700

    Use bit operator instead of for loop

--- src/main/java/basic/Exercise07.java
+++ src/main/java/basic/Exercise07.java
@@ -1,26 +1,11 @@
 package basic;
 
 public class Exercise07 {
-    public String toBinary(int n) {
-        if (n == 0) return "0";
-        StringBuilder sb = new StringBuilder();
-        for(int i = n; i > 0; i /= 2) {
-            sb.append(i % 2);
-        }
-        return sb.reverse().toString();
-    }
-
     public int toDecimalReverse(int n) {
-        final StringBuilder binaryStr = new StringBuilder(toBinary(n));
-        binaryStr.reverse();
         int decimal = 0;
-        int length = binaryStr.length();
-
-        for (int i = 0; i < length; i++) {
-            char bit = binaryStr.charAt(length - 1 - i);
-            if (bit == '1') {
-                decimal += Math.pow(2, i);
-            }
+        while (n > 0) {
+            decimal = (decimal << 1) | (n & 1);
+            n >>= 1;
         }
         return decimal;
     }
--- src/test/java/basic/Exercise07Test.java
+++ src/test/java/basic/Exercise07Test.java
@@ -9,10 +9,6 @@ public class Exercise07Test {
     @Test
     public void testToBinaryReverse() {
         final Exercise07 ex = new Exercise07();
-        assertEquals("0", ex.toBinary(0));
-        assertEquals("101", ex.toBinary(5));
-        assertEquals("1010", ex.toBinary(10));
-        assertEquals("11111111", ex.toBinary(255));
         assertEquals(255, ex.toDecimalReverse(255));
         assertEquals(5, ex.toDecimalReverse(5));
         assertEquals(29, ex.toDecimalReverse(23));
